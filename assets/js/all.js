e61031202e760b03069bfa2ac346a78fc6d7c371
李東穎 <dean19990226@gmail.com>
2022-07-22 15:47:52 +0800

    Update 2022-07-22T07:47:50.881Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,17 @@
 "use strict";
 
+// $(document).ready(() => {
+//   $('.btn__buger').on('click',function(e){
+//     $('.btn__buger').toggleClass('open');
+//   })
+// })
 console.log('Hello!');
 $(document).ready(function () {
   console.log('HesSchool Hello!');
-});
+}); // $(document).ready(() => {
+//   console.log('HesSchool Hello!');
+// });
+// $('.btn__buger').click(function(){
+//   $('btn__buger').toggleClass('open');
+// })
 //# sourceMappingURL=all.js.map
