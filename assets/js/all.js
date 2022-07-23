2bc775f07d704742b5ac50235a7cc952d574c2f8
李東穎 <dean19990226@gmail.com>
2022-07-24 00:27:21 +0800

    Update 2022-07-23T16:27:18.102Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,17 +1,17 @@
-"use strict";
-
 // $(document).ready(() => {
 //   $('.btn__buger').on('click',function(e){
 //     $('.btn__buger').toggleClass('open');
 //   })
 // })
-console.log('Hello!');
-$(document).ready(function () {
-  console.log('HesSchool Hello!');
-}); // $(document).ready(() => {
+// console.log('Hello!');
+// $(document).ready(() => {
 //   console.log('HesSchool Hello!');
 // });
-// $('.btn__buger').click(function(){
-//   $('btn__buger').toggleClass('open');
-// })
+// $(document).ready(() => {
+//     console.log('HesSchool Hello!');
+//   });
+//   $('.btn__buger').click(function(){
+//     $('btn__buger').toggleClass('open');
+//   })
+"use strict";
 //# sourceMappingURL=all.js.map
